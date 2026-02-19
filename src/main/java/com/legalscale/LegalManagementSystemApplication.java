44aeb76dab6d88c56a57cb982bbeec36e9e41f58
Nipun Bandara OU <nipunb.ou@mobitel.lk>
2026-02-19 16:14:43 +0530

    initialized a spring-boot project

---
+++ src/main/java/com/legalscale/LegalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.legalscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legalscale/legal_scale_backend/LegalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legalscale.legal_scale_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
